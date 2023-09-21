b6076b9dae5e9ceebf67e1b1c10bcb7811770ba7
Kulpot <kulpotwahu@gmail.com>
2023-09-22 06:14:49 +0800

    Add project files.

---
+++ DLL vs EXE in DotNET/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=qwC4KRWHBdk&list=PLRwVmtr-pp06rfSgNYu_oBg40DkwXiRHt&index=8
+//
+
+/*---------------------------------CMD--------------------------------------
+ * 
+C:\Users\sunny\source\repos\DLL vs EXE in DotNET>cd DLL vs EXE in DotNet
+
+C:\Users\sunny\source\repos\DLL vs EXE in DotNET\DLL vs EXE in DotNET>dir
+ Volume in drive C has no label.
+ Volume Serial Number is DAE4-938D
+
+ Directory of C:\Users\sunny\source\repos\DLL vs EXE in DotNET\DLL vs EXE in DotNET
+
+22/09/2023  06:12 am    <DIR>          .
+22/09/2023  06:12 am    <DIR>          ..
+22/09/2023  05:42 am               189 App.config
+22/09/2023  05:42 am    <DIR>          bin
+22/09/2023  05:43 am             2,390 DLL vs EXE in DotNET.csproj
+22/09/2023  05:42 am    <DIR>          obj
+22/09/2023  06:12 am               232 Program.cs
+22/09/2023  05:42 am    <DIR>          Properties
+               3 File(s)          2,811 bytes
+               5 Dir(s)  489,869,049,856 bytes free
+
+
+
+
+ * 
+ */
+
+
+
+class MainClass
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello world");
+    }
+}
